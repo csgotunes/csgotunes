4aa0549cca7cad38d96888448e818c79ce8a2953
Anthony Iacono <anthonyiacono@gmail.com>
2022-09-25 15:46:02 -0400

    Website: Add textarea with config contents and copy to clipboard button
    As recommended by issue #14, this commit adds a textarea with the contents of the CFG file (as well as a button to copy to clipboard) which allows users to create the gamestate integration configuration themselves if they are not fond on downloading the automatically generated file.

--- Website/Source/Pages/DashboardPage.tsx
+++ Website/Source/Pages/DashboardPage.tsx
@@ -6,14 +6,14 @@ import FileSaver from 'file-saver';
 import { clearSession, getSession } from '../Utils/AuthUtils';
 import { useTranslation } from 'react-i18next';
 import { UpdateUserProfileRequest } from '../Models/UpdateUserProfileRequest';
+import copy from 'copy-to-clipboard';
 
 export const DashboardPage: React.FunctionComponent<any> = () => {
   const history = useHistory();
   const [isLoading, setLoading] = useState(false);
   const [isDisabled, setDisabled] = useState(false);
   const [isSettingDisabledPreference, setSettingDisabledPreference] = useState(false);
-  const [spotifyUserID, setSpotifyUserID] = useState<string | null>(null);
-  const [cfgKey, setCFGKey] = useState<string | null>(null);
+  const [configContents, setConfigContents] = useState<string | null>(null);
 
   const { t } = useTranslation();
 
@@ -50,9 +50,37 @@ export const DashboardPage: React.FunctionComponent<any> = () => {
         }
 
         const userProfileResponse: UserProfileResponse = await response.json();
-        setSpotifyUserID(userProfileResponse.spotifyUserID);
-        setCFGKey(userProfileResponse.cfgKey);
         setDisabled(userProfileResponse.isDisabled);
+
+        const encodedSpotifyUserId = encodeURIComponent(userProfileResponse.spotifyUserID);
+        const encodedCFGKey = encodeURIComponent(userProfileResponse.cfgKey);
+
+        const userConfig = `"CS:GO Tunes"
+{
+ "uri" "https://csgotunes.azurewebsites.net/api/game-state?spotifyUserID=${encodedSpotifyUserId}&cfgKey=${encodedCFGKey}"
+ "timeout" "5.0"
+ "buffer"  "0.1"
+ "throttle" "0.5"
+ "heartbeat" "60.0"
+ "output"
+ {
+   "precision_time" "3"
+   "precision_position" "1"
+   "precision_vector" "3"
+ }
+ "data"
+ {
+   "provider"            "1"      // general info about client being listened to: game name, appid, client steamid, etc.
+   "map"                 "1"      // map, gamemode, and current match phase ('warmup', 'intermission', 'gameover', 'live') and current score
+   "round"               "1"      // round phase ('freezetime', 'over', 'live'), bomb state ('planted', 'exploded', 'defused'), and round winner (if any)
+   "player_id"           "1"      // player name, clan tag, observer slot (ie key to press to observe this player) and team
+   "player_state"        "1"      // player state for this current round such as health, armor, kills this round, etc.
+   "player_weapons"      "1"      // output equipped weapons.
+   "player_match_stats"  "1"      // player stats this match such as kill, assists, score, deaths and MVPs
+ }
+}`;
+
+        setConfigContents(userConfig);
       } finally {
         setLoading(false);
       }
@@ -96,49 +124,30 @@ export const DashboardPage: React.FunctionComponent<any> = () => {
   };
 
   const downloadConfig = (e: MouseEvent<HTMLButtonElement>): void => {
-    if (spotifyUserID === null || cfgKey === null) {
+    if (configContents === null) {
       return;
     }
 
-    const encodedSpotifyUserId = encodeURIComponent(spotifyUserID);
-    const encodedCFGKey = encodeURIComponent(cfgKey);
-
-    const userConfig = `
-"CS:GO Tunes"
-{
- "uri" "https://csgotunes.azurewebsites.net/api/game-state?spotifyUserID=${encodedSpotifyUserId}&cfgKey=${encodedCFGKey}"
- "timeout" "5.0"
- "buffer"  "0.1"
- "throttle" "0.5"
- "heartbeat" "60.0"
- "output"
- {
-   "precision_time" "3"
-   "precision_position" "1"
-   "precision_vector" "3"
- }
- "data"
- {
-   "provider"            "1"      // general info about client being listened to: game name, appid, client steamid, etc.
-   "map"                 "1"      // map, gamemode, and current match phase ('warmup', 'intermission', 'gameover', 'live') and current score
-   "round"               "1"      // round phase ('freezetime', 'over', 'live'), bomb state ('planted', 'exploded', 'defused'), and round winner (if any)
-   "player_id"           "1"      // player name, clan tag, observer slot (ie key to press to observe this player) and team
-   "player_state"        "1"      // player state for this current round such as health, armor, kills this round, etc.
-   "player_weapons"      "1"      // output equipped weapons.
-   "player_match_stats"  "1"      // player stats this match such as kill, assists, score, deaths and MVPs
- }
-}
-        `;
-
-    const blob = new Blob([userConfig], {
+    const blob = new Blob([configContents], {
       type: 'text/plain;charset=utf-8'
     });
     FileSaver.saveAs(blob, 'gamestate_integration_csgotunes.cfg');
   };
+
+  const copyConfig = (e: MouseEvent<HTMLButtonElement>): void => {
+    if (configContents === null) {
+      return;
+    }
+
+    copy(configContents);
+  };
   return (
     <div>
       <p>{t('dashboard_welcome')}</p>
-      <button onClick={downloadConfig} disabled={isLoading}>{t('download_cfg_button')}</button>
+      <div>
+        <button onClick={downloadConfig} disabled={isLoading}>{t('download_cfg_button')}</button>
+      </div>
+      <div>
       {isDisabled
         ? (
             <button onClick={(e: MouseEvent<HTMLButtonElement>) => setDisabledPreference(false)} disabled={isLoading}>{t('enable_button')}</button>
@@ -146,6 +155,13 @@ export const DashboardPage: React.FunctionComponent<any> = () => {
         : (
         <button onClick={(e: MouseEvent<HTMLButtonElement>) => setDisabledPreference(true)} disabled={isLoading || isSettingDisabledPreference}>{t('disable_button')}</button>
           )}
+      </div>
+      <div>
+        <textarea value={configContents === null ? '' : configContents} readOnly={true} />
+      </div>
+      <div>
+        <button onClick={copyConfig} disabled={isLoading}>{t('copy_cfg_to_clipboard_button')}</button>
+      </div>
     </div>
   );
 };
